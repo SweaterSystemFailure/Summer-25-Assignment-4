e074cc9a02a73f8bd1866c5db9761408787b9d7b
Ben <benjamin.s.fagan@gmail.com>
2025-06-18 14:05:14 -0700

    added header & source files for functions

---
+++ Functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+void mainMenu();
+void addItem();
+void displayItems();
+unsigned unsignedValidator(const string& prompt, unsigned min, unsigned max);
--- Summer 25 Assignment 4.cpp
+++ Summer 25 Assignment 4.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
+#include "functions.h"
 using namespace std;
 
-void mainMenu();
-void addItem();
-void displayItems();
-unsigned unsignedValidator(const string& prompt, unsigned min, unsigned max);
 
 int main()
 {
@@ -12,61 +9,4 @@ int main()
    mainMenu();
 
    return 0;
-}
-
-void mainMenu() {
-		cout << "Would you like to: " << endl;
-		cout << "1. Add an item." << endl;
-		cout << "2. View items." << endl;
-		cout << "3. Exit" << endl;
-
-		switch (unsignedValidator("Please make your selection: [1-3]", 1, 3)) {
-		case 1:
-			addItem();
-			mainMenu();
-			break;
-		case 2:
-			displayItems();
-			mainMenu();
-			break;
-		case 3:
-			cout << "Exiting...";
-			cout << endl;
-			break;
-		default:
-			cout << "It looks like something went wrong. Let's try again." << endl;
-			cout << endl;
-		}
-}
-
-void addItem(){
-	cout << "Item added!" << endl;
-	cout << endl;
-}
-
-void displayItems() {
-	cout << "Displaying Items..." << endl;
-	cout << endl;
-}
-
-unsigned unsignedValidator(const string& prompt, unsigned min, unsigned max) {
-	unsigned number;
-	while (true) {
-		cout << prompt << endl;
-		cin >> number;
-		if (cin.fail()) {
-			cout << "Enter a valid number." << endl;
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-		else if (number < min || number > max) {
-			cout << "Number must be between " << min << " and " << max << "." << endl;
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-		else {
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			break;
-		}
-	}
-	return number;
 }
